7d745f774f07c4904521d819b93937c09ae9d953
MichalKrywult <218646090+MichalKrywult@users.noreply.github.com>
2026-06-03 19:44:59 +0200

    HOTFIX: remove double User table creation

--- server/app/shopping/models.py
+++ server/app/shopping/models.py
@@ -3,18 +3,6 @@ from typing import ClassVar, List, Optional
 from sqlmodel import Field, Relationship, SQLModel
 
 
-class User(SQLModel, table=True):
-    __tablename__: ClassVar[str] = "users"
-    id: Optional[int] = Field(default=None, primary_key=True, index=True)
-    username: str = Field(unique=True, index=True, nullable=False)
-    hashed_password: str = Field(nullable=False)
-    is_active: bool = Field(default=True)
-    shopping_items: List["Item"] = Relationship(
-        back_populates="owner",
-        sa_relationship_kwargs={"cascade": "all, delete-orphan"},
-    )
-
-
 class ShoppingList(SQLModel, table=True):
     __tablename__: ClassVar[str] = "shopping_lists"
     id: Optional[int] = Field(default=None, primary_key=True)
