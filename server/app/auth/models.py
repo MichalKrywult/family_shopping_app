5df5f129f510a0444c8a8f925eb86f4e07f8f049
MichalKrywult <218646090+MichalKrywult@users.noreply.github.com>
2026-06-04 11:46:48 +0200

    Add: add user registration and JWT login

--- server/app/auth/models.py
+++ server/app/auth/models.py
@@ -7,9 +7,17 @@ class User(SQLModel, table=True):
 
     id: Optional[int] = Field(default=None, primary_key=True, index=True)
     username: str = Field(unique=True, index=True, nullable=False)
+    email: Optional[str] = Field(default=None, unique=True, index=True, nullable=True)
+
     hashed_password: str = Field(nullable=False)
     is_active: bool = Field(default=True)
 
     shopping_items: List["Item"] = Relationship(
         back_populates="owner", sa_relationship_kwargs={"cascade": "all, delete-orphan"}
     )
+
+
+class UserCreate(SQLModel):
+    username: str
+    password: str
+    email: str
